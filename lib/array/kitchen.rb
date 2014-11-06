d098eb01bc31cfc3538747f0aeb691125d2a16f7
Daniel Hollands <daniel.hollands@intercity-uk.com>
2014-11-06 14:27:02 +0000

    array ahoy

--- lib/array/kitchen.rb
+++ lib/array/kitchen.rb
@@ -4,6 +4,42 @@ class Kitchen
   end
 
   def items
-    @tiems
+    @items
+  end
+
+  def add(item)
+    @items << item
+  end
+
+  def remove_first_and_last_elements
+    @items.pop
+    @items.shift
+    @items
+  end
+
+  def add_bang
+    @items.map! { |element| "#{element}!" }
+  end
+
+  def remove_with_slice(array)
+    remove = %w(rat shoe)
+    array - remove
+  end
+
+  def make_mini_arrays(array)
+    array.map { |element| [element] }
+  end
+
+  def remove_if_more_than_three_letters(array)
+    array.delete_if { |element| element.length > 3 }
+  end
+
+  def alphabetically_backwards(array)
+    array.sort_by! { |element| element }
+    array.reverse
+  end
+
+  def first_three(array)
+    array.first(3)
   end
 end
